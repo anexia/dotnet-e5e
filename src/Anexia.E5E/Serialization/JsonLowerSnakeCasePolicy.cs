f7ed155f73ed4c7833d141c84a298e85fee6ec42
Jasmin Oster <joster@anexia.com>
2023-10-27 12:42:45 +0200

    Make snake_case conversion culture invariant

--- src/Anexia.E5E/Serialization/JsonLowerSnakeCasePolicy.cs
+++ src/Anexia.E5E/Serialization/JsonLowerSnakeCasePolicy.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Text.Json;
 
 namespace Anexia.E5E.Serialization;
@@ -7,6 +8,6 @@ internal class JsonLowerSnakeCasePolicy : JsonNamingPolicy
 	public override string ConvertName(string name)
 	{
 		return string.Concat(name.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x : x.ToString()))
-			.ToLower();
+			.ToLower(CultureInfo.InvariantCulture);
 	}
 }
