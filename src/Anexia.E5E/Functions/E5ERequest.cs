1b1711a82f0c1b9375d055fb9041ef11e3f636bd
Jasmin Oster <joster@anexia.com>
2023-10-30 17:26:14 +0100

    Make event and context non-nullable

--- src/Anexia.E5E/Functions/E5ERequest.cs
+++ src/Anexia.E5E/Functions/E5ERequest.cs
@@ -5,4 +5,4 @@ namespace Anexia.E5E.Functions;
 /// <param name="Event">Contains the user-provided request information, e.g. HTTP headers, the payload and other information.</param>
 /// <param name="Context">Contains E5E-provided metadata about the current execution.</param>
 /// </summary>
-public record E5ERequest(E5EEvent? Event, E5ERequestContext? Context);
+public record E5ERequest(E5EEvent Event, E5ERequestContext Context);
