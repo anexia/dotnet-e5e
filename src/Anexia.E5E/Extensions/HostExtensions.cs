96018eea5b2766a4a1f7f828cb65aaca0d6b2d13
Jasmin Oster <joster@anexia.com>
2023-11-15 13:46:11 +0100

    Add method to register any type as an entrypoint

--- src/Anexia.E5E/Extensions/HostExtensions.cs
+++ src/Anexia.E5E/Extensions/HostExtensions.cs
@@ -20,12 +20,25 @@ public static class HostExtensions
 	/// <param name="host">The host.</param>
 	/// <param name="entrypoint">The name of the entrypoint.</param>
 	/// <typeparam name="T">The type of the handler.</typeparam>
-	public static void RegisterEntrypoint<T>(this IHost host, string entrypoint) where T : IE5EFunctionHandler
+	public static void RegisterEntrypoint<T>(this IHost host, string entrypoint) where T : IE5EFunctionHandler => RegisterEntrypoint(host, entrypoint, typeof(T));
+
+	/// <summary>
+	/// Register an entrypoint with the given handler type.
+	/// </summary>
+	/// <param name="host">The host.</param>
+	/// <param name="entrypoint">The name of the entrypoint.</param>
+	/// <param name="handlerType">The type of the class, needs to implement <see cref="IE5EFunctionHandler"/>.</param>
+	/// <exception cref="InvalidOperationException">Thrown if the handlerType is neither a class nor does it implement <see cref="IE5EFunctionHandler"/>.</exception>
+	public static void RegisterEntrypoint(this IHost host, string entrypoint, Type handlerType) 
 	{
 		ArgumentNullException.ThrowIfNull(host);
+		ArgumentNullException.ThrowIfNull(handlerType);
+		
+		if (!handlerType.IsClass || !handlerType.IsAssignableTo(typeof(IE5EFunctionHandler)))
+			throw new InvalidOperationException($"The type {handlerType} is not suitable for registration.");
 
 		var resolver = host.Services.GetRequiredService<E5EFunctionHandlerResolver>();
-		resolver.Add(entrypoint, typeof(T));
+		resolver.Add(entrypoint, handlerType);
 	}
 
 	/// <summary>
